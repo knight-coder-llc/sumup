4056979a4bac0b6a66715f45d012dc64e87d2774
reefman001 <brian.kilburn2@yahoo.com>
2020-11-12 00:20:26 -0500

    added request token functionality, not tested.

--- src/Controller/SumUpController.php
+++ src/Controller/SumUpController.php
@@ -3,7 +3,9 @@
 namespace Drupal\sumup;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\State\StateInterface;
+use Drupal\Core\State\State;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\sumup\SumUpOAuth2Service;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -12,6 +14,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * Controller for sumup module.
  */
 class SumUpController extends ControllerBase {
+
     /**
      * @var \Guzzle\Client
      */
@@ -24,58 +27,97 @@ class SumUpController extends ControllerBase {
 
     /**
      * The serializer
+     * 
      * @var \Symfony\Component\Serializer\Serializer
      */
     protected $serializer;
+
+    /**
+     * The state system.
+     * 
+     * @var Drupal\Core\State\State;
+     */
+    protected $state_system;
+
+    /**
+     * configuration settings.
+     * 
+     * @var Drupal\Core\Config\ConfigFactoryInterface;
+     */
+    protected $config_factory;
+
+    /**
+     * The sumup authorization service.
+     * 
+     * @var Drupal\sumup\SumUpOAuth2Service;
+     */
+    protected $sumup_service;
 
     /**
      * Constructor method for SumUpController.
      * 
      * @param
      */
-    public function __construct(Client $http_client, RequestStack $request_stack, Serializer $serializer) {
+    public function __construct(
+        Client $http_client, 
+        RequestStack $request_stack, 
+        Serializer $serializer, 
+        State $state_system, 
+        ConfigFactoryInterface $config_factory, 
+        SumUpOAuth2Servie $sumup_service) {
+
+        $this->config_factory = $config_factory->get('sumup.registered_app_settings');
         $this->http_client = $http_client;
         $this->request_stack = $request_stack;
         $this->serializer = $serializer;
+        $this->state_system = $state_system;
+        $this->sumup_service = $sumup_service;
+
     }   
 
     /**
-     * request granted redirect
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container) {
+        return new static(
+            $container->get('http_client'),
+            $container->get('request_stack'),
+            $container->get('serializer'),
+            $container->get('state'),
+            $container->get('config.factory'),
+            $container->get('sumup.oauth2_authenticate')
+        );
+    }
+
+    /**
+     * Authorization application callback.
      */
     public function oauth_sumup_callback() {
+
+        $config = $this->config_factory;
         $request_stack = $this->request_stack;
         $request = $request_stack->getCurrentRequest();
-        
-        /**
-         * code
-         * state
-         */
+        $sumup_service = $this->sumup_service;
+
         $payload = $this->decode(
             $request->getContent(),
             $request->getContentType()
         );
 
-        $headers = $request->headers->all();
-        // $request->headers->get('Content-Type')
+        // request access token.
+        /** Incoming: code, state, error */
+        if(isset($payload['code'])) {
+            $code = $payload['code'];
+            $sumup_service->requestToken($code); 
+        }
+        
+        // store the payload.
+        /** Incoming: access_token, token_type, expires_in, refresh_token */
+        if(isset($payload['access_token'])) {
+            // encrption services?
+            $sumup_service->saveAccessToken($payload);    
+        }
 
-        /**
-         * request access token
-         * 
-         * grant_type
-         * client_id
-         * client_secret
-         * redirect_uri
-         * code
-         * 
-         * function somefunction($code)
-         * 
-         * access_token
-         * token_type
-         * expires_in
-         * refresh_token
-         * 
-         * request resources
-         */
         return;
     }
 
--- src/SumUpOAuth2Service.php
+++ src/SumUpOAuth2Service.php
@@ -3,6 +3,7 @@
 namespace Drupal\sumup;
 
 use GuzzleHttp\Client;
+use Drupal\Core\State\State;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 
@@ -22,10 +23,17 @@ class SumUpOAuth2Service {
      */
     protected $http_client;
 
-    public function __construct(ConfigFactoryInterface $config_factory, Client $http_client){
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $state_system;
+
+    public function __construct(ConfigFactoryInterface $config_factory, Client $http_client, State $state_system){
         $this->config_factory = $config_factory;
         $this->http_client = $http_client;
+        $this->state_system = $state_system;
     }
+
     /**
      * Request scope access confirmation.
      */
@@ -36,37 +44,64 @@ class SumUpOAuth2Service {
 
         $client_id = $config->get('sumup_client_id');
         $redirect_uri = $config->get('sumup_redirect_uri');
-        $scope = $config->get('sumup_application_scopes');
-        /**
-         * response_type
-         * client_id
-         * redirect_uri
-         * scope
-         * state
-         */
+        $scopes = $config->get('sumup_application_scopes');
+
+        /** response_type, client_id, redirect_uri, scope, state */
         $opts = array(
             'response_type' => 'code',
             'client_id' => $client_id,
             'redirect_uri' => $redirect_uri,
-            'scope' => $scope
+            'scope' => $scopes
+        );
+
+        $response = $client->request('GET', 'https://api.sumup.com/authorize', $opts);
+        
+        if($response->getStatusCode() != 200) {
+            \Drupal::logger('sumup', print_r('Response Status: ' . $response->getStatusCode(), true));
+            return;
+        }
+
+        return;
+    }
+
+    /**
+     * Request access token  with authorization code. 
+     * 
+     * @var integer
+     */
+    public function requestToken($code) {
+        $config = $this->config_factory('sumup.registered_app_settings');
+        $client = $this->http_client;
+
+        $client_id = $config->get('sumup_client_id');
+        $client_secret = $config->get('sumup_client_secret');
+        $redirect_uri = $config->get('sumup_redirect_uri');
+
+        /** grant_type, client_id, client_secret, redirect_uri, code */
+        $post_data = array(
+            'grant_type' => 'authorization_code',
+            'client_id' => $client_id,
+            'client_secret' => $client_secret,
+            'redirect_uri' => $redirect_uri,
+            'code' => $code
         );
 
         \Drupal::logger('sumup', print_r('Options: ' . $opts, true));
 
-        // $response = $client->get('https://api.sumup.com/authorize', $opts);
+        $response = $client->request('POST','https://api.sumup.com/token', [
+            'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
+            'body' => $post_data
+        ]);
         
-        // if($response->getStatusCode() != 200) {
-        //     \Drupal::logger('sumup', print_r('Response Status: ' . $response->getStatusCode(), true));
-        //     return;
-        // }
+        if($response->getStatusCode() != 200) {
+            \Drupal::logger('sumup', print_r('Response Status: ' . $response->getStatusCode(), true));
+            return;
+        }
 
-        // \Drupal::logger('sumup', print_r('Response Status: ' . $response->getStatusCode(), true));
-
+        return;
     }
 
-    public function requestToken() {}
-
     public function validateResponse() {}
-
-    protected function saveAccessToken() {}
+    //TODO: figure out the refresh token cycle.
+    public function saveAccessToken($payload) {}
 }
